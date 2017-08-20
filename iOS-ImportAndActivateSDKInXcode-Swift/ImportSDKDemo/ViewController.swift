59a5d699ae2cca4deea629270bf1875d4e899ba1
新川盛幸 <arakawamoriyuki@gmail.com>
2017-08-20 16:21:18 +0900

    added product controll button

--- iOS-ImportAndActivateSDKInXcode-Swift/ImportSDKDemo/ViewController.swift
+++ iOS-ImportAndActivateSDKInXcode-Swift/ImportSDKDemo/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import DJISDK
 
 class ViewController: UIViewController, DJISDKManagerDelegate {
-
+    
     // viewの呼び出し 1度のみ (UIViewController)
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +27,8 @@ class ViewController: UIViewController, DJISDKManagerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
     
     // info.plistのDJISDKAppKey検証後 (DJISDKManagerDelegate)
     func appRegisteredWithError(_ error: Error?) {
@@ -45,16 +47,66 @@ class ViewController: UIViewController, DJISDKManagerDelegate {
     func productConnected(_ product: DJIBaseProduct?) {
         
         NSLog("Product Connected")
-        self.showAlertViewWithTitle(title:"Register App", withMessage: "Product Connected")
+        self.showAlertViewWithTitle(title:"Product Connection", withMessage: "Product Connected")
     }
     
     // dji製品と接続解除時 (DJISDKManagerDelegate)
     func productDisconnected() {
         
         NSLog("Product Disconnected")
-        self.showAlertViewWithTitle(title:"Register App", withMessage: "Disconnected")
+        self.showAlertViewWithTitle(title:"Product Connection", withMessage: "Disconnected")
     }
 
+    
+    
+    // モータースタート
+    @IBAction func onTurnOnMotorsButtonTouchUp(_ sender: Any) {
+        
+        NSLog("onTurnOnMotorButtonTouchUp")
+        
+        if let aircraft = DJISDKManager.product() as? DJIAircraft {
+            aircraft.flightController?.turnOnMotors(completion: nil)
+        }else{
+            self.showAlertViewWithTitle(title:"Action", withMessage: "Product Connection Not Found")
+        }
+    }
+    
+    // モーターストップ
+    @IBAction func onTurnOffMotorsButtonTouchUp(_ sender: Any) {
+        
+        NSLog("onTurnOffMotorButtonTouchUp")
+        
+        if let aircraft = DJISDKManager.product() as? DJIAircraft {
+            aircraft.flightController?.turnOffMotors(completion: nil)
+        }else{
+            self.showAlertViewWithTitle(title:"Action", withMessage: "Product Connection Not Found")
+        }
+    }
+    
+    // 離陸
+    @IBAction func onTakeOffButtonTouchUp(_ sender: Any) {
+        
+        NSLog("onTakeOffButtonTouchUp")
+        
+//        if let aircraft = DJISDKManager.product() as? DJIAircraft {
+//            aircraft.flightController?.startTakeoff(completion: nil)
+//        }else{
+//            self.showAlertViewWithTitle(title:"Action", withMessage: "Product Connection Not Found")
+//        }
+    }
+    
+    // 着陸
+    @IBAction func onLandingButtonTouchUp(_ sender: Any) {
+        
+        NSLog("onLandingButtonTouchUp")
+        
+//        if let aircraft = DJISDKManager.product() as? DJIAircraft {
+//            aircraft.flightController?.startLanding(completion: nil)
+//        }else{
+//            self.showAlertViewWithTitle(title:"Action", withMessage: "Product Connection Not Found")
+//        }
+    }
+    
     
     
     // アラート表示
@@ -64,35 +116,5 @@ class ViewController: UIViewController, DJISDKManagerDelegate {
         alert.addAction(okAction)
         self.present(alert, animated: true, completion: nil)
     }
-    
-    // モータースタート
-    func turnOnMotors(){
-        if let aircraft = DJISDKManager.product() as? DJIAircraft {
-            aircraft.flightController?.turnOnMotors(completion: nil)
-        }
-    }
-    
-    // モーターストップ
-    func turnOffMotors(){
-        if let aircraft = DJISDKManager.product() as? DJIAircraft {
-            aircraft.flightController?.turnOffMotors(completion: nil)
-        }
-    }
-    
-    // 離陸
-    func takeOff(){
-        if let aircraft = DJISDKManager.product() as? DJIAircraft {
-            aircraft.flightController?.startTakeoff(completion: nil)
-        }
-    }
-    
-    // 着陸
-    func landing(){
-        if let aircraft = DJISDKManager.product() as? DJIAircraft {
-            aircraft.flightController?.startLanding(completion: nil)
-        }
-    }
-    
-    
 }
 
