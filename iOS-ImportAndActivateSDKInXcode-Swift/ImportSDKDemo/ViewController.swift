db2d2686ad72ad158d4aa656445d68a6747dfef8
新川盛幸 <arakawamoriyuki@gmail.com>
2017-08-20 15:41:09 +0900

    added product controll function

--- iOS-ImportAndActivateSDKInXcode-Swift/ImportSDKDemo/ViewController.swift
+++ iOS-ImportAndActivateSDKInXcode-Swift/ImportSDKDemo/ViewController.swift
@@ -11,40 +11,24 @@ import DJISDK
 
 class ViewController: UIViewController, DJISDKManagerDelegate {
 
+    // viewの呼び出し 1度のみ (UIViewController)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // viewの表示完了後 (UIViewController)
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         DJISDKManager.registerApp(with: self)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
+    // 低メモリ時 (UIViewController)
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func showAlertViewWithTitle(title: String, withMessage message: String) {
-        
-        let alert = UIAlertController.init(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-        let okAction = UIAlertAction.init(title:"OK", style: UIAlertActionStyle.default, handler: nil)
-        alert.addAction(okAction)
-        self.present(alert, animated: true, completion: nil)
-        
-    }
-    
-    // DJISDKManagerDelegate Methods
-    func productConnected(_ product: DJIBaseProduct?) {
-        
-        NSLog("Product Connected")
-    }
-    
-    func productDisconnected() {
-        
-        NSLog("Product Disconnected")
-    }
-    
+    // info.plistのDJISDKAppKey検証後 (DJISDKManagerDelegate)
     func appRegisteredWithError(_ error: Error?) {
         
         var message = "Register App Successed!"
@@ -53,10 +37,62 @@ class ViewController: UIViewController, DJISDKManagerDelegate {
         }else{
             NSLog("Register App Successed!");
         }
-
+        
         self.showAlertViewWithTitle(title:"Register App", withMessage: message)
     }
+    
+    // dji製品と接続時 (DJISDKManagerDelegate)
+    func productConnected(_ product: DJIBaseProduct?) {
+        
+        NSLog("Product Connected")
+        self.showAlertViewWithTitle(title:"Register App", withMessage: "Product Connected")
+    }
+    
+    // dji製品と接続解除時 (DJISDKManagerDelegate)
+    func productDisconnected() {
+        
+        NSLog("Product Disconnected")
+        self.showAlertViewWithTitle(title:"Register App", withMessage: "Disconnected")
+    }
 
-
+    
+    
+    // アラート表示
+    func showAlertViewWithTitle(title: String, withMessage message: String) {
+        let alert = UIAlertController.init(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction.init(title:"OK", style: UIAlertActionStyle.default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // モータースタート
+    func turnOnMotors(){
+        if let aircraft = DJISDKManager.product() as? DJIAircraft {
+            aircraft.flightController?.turnOnMotors(completion: nil)
+        }
+    }
+    
+    // モーターストップ
+    func turnOffMotors(){
+        if let aircraft = DJISDKManager.product() as? DJIAircraft {
+            aircraft.flightController?.turnOffMotors(completion: nil)
+        }
+    }
+    
+    // 離陸
+    func takeOff(){
+        if let aircraft = DJISDKManager.product() as? DJIAircraft {
+            aircraft.flightController?.startTakeoff(completion: nil)
+        }
+    }
+    
+    // 着陸
+    func landing(){
+        if let aircraft = DJISDKManager.product() as? DJIAircraft {
+            aircraft.flightController?.startLanding(completion: nil)
+        }
+    }
+    
+    
 }
 
